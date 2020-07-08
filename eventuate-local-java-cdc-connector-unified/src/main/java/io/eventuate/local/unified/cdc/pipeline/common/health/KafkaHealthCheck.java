a3a134894c5346d2e4b3c4e25f622a75c5958d95
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-07-08 15:36:18 +0300

    #60, 61 - added synchronization to kafka healthckeck and made it configurable.

--- eventuate-local-java-cdc-connector-unified/src/main/java/io/eventuate/local/unified/cdc/pipeline/common/health/KafkaHealthCheck.java
+++ eventuate-local-java-cdc-connector-unified/src/main/java/io/eventuate/local/unified/cdc/pipeline/common/health/KafkaHealthCheck.java
@@ -1,9 +1,11 @@
 package io.eventuate.local.unified.cdc.pipeline.common.health;
 
 import io.eventuate.messaging.kafka.basic.consumer.ConsumerPropertiesFactory;
+import io.eventuate.messaging.kafka.spring.basic.consumer.EventuateKafkaConsumerSpringConfigurationProperties;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import java.util.Properties;
 import java.util.UUID;
@@ -14,20 +16,30 @@ public class KafkaHealthCheck extends AbstractHealthCheck {
   @Value("${eventuatelocal.kafka.bootstrap.servers}")
   private String kafkaServers;
 
-  private KafkaConsumer<String, String> consumer;
+  @Autowired
+  private EventuateKafkaConsumerSpringConfigurationProperties eventuateKafkaConsumerSpringConfigurationProperties;
+
+  private volatile KafkaConsumer<String, String> consumer;
 
   @Override
   protected void determineHealth(HealthBuilder builder) {
     if (consumer == null) {
-      Properties consumerProperties = ConsumerPropertiesFactory.makeDefaultConsumerProperties(kafkaServers, UUID.randomUUID().toString());
-      consumerProperties.put("session.timeout.ms", "500");
-      consumerProperties.put("request.timeout.ms", "1000");
-      consumerProperties.put("heartbeat.interval.ms", "100");
-      consumer = new KafkaConsumer<>(consumerProperties);
+      synchronized (this) {
+        if (consumer == null) {
+          Properties consumerProperties = ConsumerPropertiesFactory.makeDefaultConsumerProperties(kafkaServers, UUID.randomUUID().toString());
+          consumerProperties.put("session.timeout.ms", "500");
+          consumerProperties.put("request.timeout.ms", "1000");
+          consumerProperties.put("heartbeat.interval.ms", "100");
+          consumerProperties.putAll(eventuateKafkaConsumerSpringConfigurationProperties.getProperties());
+          consumer = new KafkaConsumer<>(consumerProperties);
+        }
+      }
     }
 
     try {
-      consumer.partitionsFor("__consumer_offsets");
+      synchronized (this) {
+        consumer.partitionsFor("__consumer_offsets");
+      }
       builder.addDetail("Connected to Kafka");
     } catch (Exception e) {
       logger.error(e.getMessage(), e);
