8aaa3cd3fbd135a161685a72fe1f9a31c2147d68
Chris Richardson <chris@chrisrichardson.net>
2022-02-08 09:37:36 -0800

    Use DOCKER_HOST_NAME to override host.docker.internal

--- eventuate-cdc-service/build-docker-multi-arch.sh
+++ eventuate-cdc-service/build-docker-multi-arch.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd $( dirname "${BASH_SOURCE[0]}" ) ; pwd)
 docker-compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
 
 docker buildx build --platform linux/amd64,linux/arm64 \
-  -t ${MYSQL8_MULTI_ARCH_IMAGE:-host.docker.internal:5002/eventuate-cdc-service:multi-arch-local-build} \
+  -t ${MYSQL8_MULTI_ARCH_IMAGE:-${DOCKER_HOST_NAME:-host.docker.internal}:5002/eventuate-cdc-service:multi-arch-local-build} \
   -f $SCRIPT_DIR/Dockerfile \
   ${BUILDX_PUSH_OPTIONS:---output=type=image,push=true,registry.insecure=true} \
   $SCRIPT_DIR
