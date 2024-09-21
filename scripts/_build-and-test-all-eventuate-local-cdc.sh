d71351e2843ae1f71dbf2b5009e9a59c0e01cae6
Chris Richardson <chris@chrisrichardson.net>
2024-09-21 14:19:04 +0900

    Misc changes.

--- scripts/_build-and-test-all-eventuate-local-cdc.sh
+++ scripts/_build-and-test-all-eventuate-local-cdc.sh
@@ -10,11 +10,11 @@ export EVENTUATE_CDC_TYPE=EventuateLocal
 
 ./gradlew $GRADLE_OPTIONS :eventuate-local-java-cdc-connector-e2e-tests:cleanTest :eventuate-local-java-cdc-connector-e2e-tests:test
 
-echo testing database and zookeeper restart scenario $(date)
+# echo testing database and zookeeper restart scenario $(date)
 
-./gradlew -P composeServices=zookeeper,kafka,${DATABASE} :eventuate-local-java-cdc-connector-e2e-tests:eventuatelocalcdcComposeDown
-sleep 10
+# ./gradlew -P composeServices=zookeeper,kafka,${DATABASE} :eventuate-local-java-cdc-connector-e2e-tests:eventuatelocalcdcComposeDown
+# sleep 10
 
-./gradlew $GRADLE_OPTIONS :eventuate-local-java-cdc-connector-e2e-tests:cleanTest :eventuate-local-java-cdc-connector-e2e-tests:test
+# ./gradlew $GRADLE_OPTIONS :eventuate-local-java-cdc-connector-e2e-tests:cleanTest :eventuate-local-java-cdc-connector-e2e-tests:test
 
-./gradlew :eventuate-local-java-cdc-connector-e2e-tests:eventuatelocalcdcComposeDown
+# ./gradlew :eventuate-local-java-cdc-connector-e2e-tests:eventuatelocalcdcComposeDown
