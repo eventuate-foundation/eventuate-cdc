533e4141f1db7aabe912646d4939e0c4a2938973
Chris Richardson <chris@chrisrichardson.net>
2025-07-10 16:17:27 +0900

    docker-compose -> docker compose

--- eventuate-cdc-service/build-docker-multi-arch.sh
+++ eventuate-cdc-service/build-docker-multi-arch.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $( dirname "${BASH_SOURCE[0]}" ) ; pwd)
 
-docker-compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
+docker compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
 
 docker buildx build --platform linux/amd64,linux/arm64 \
   -t ${EVENTUATE_CDC_SERVICE_MULTI_ARCH_IMAGE:-${DOCKER_HOST_NAME:-host.docker.internal}:5002/eventuate-cdc-service:multi-arch-local-build} \
